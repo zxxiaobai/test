003f144a2863e9faa5b08c3ae8da6fb72cba019f
zxxiaobai <648236798@qq.com>
2020-05-10 23:43:32 +0800

    学习第8天,开始准备进入合并阶段

--- js/day7.js
+++ js/day7.js
@@ -1,3 +1,23 @@
 $(function () {
+ $("#cllbtn").click(function () {
+     //$("#clp4").collapse(); 不加参数 第一次展开后再次点击不会关闭
+     //$("#clp4").collapse("toggle");重复点击会打开关闭
+     $("#clp4").collapse({
+         parent:$("#pg"),//获取该父类赋值parent
 
+     }).collapse("toggle");
+     //下面是绑定事件的操作，调用on方法
+     $("#clp4").on("show.bs.collapse",function () {//给这个事件绑定个函数
+         alert("我去哦");
+     });
+     $("#clp4").on("shown.bs.collapse",function () {//给这个事件绑定个函数
+         alert("我去哦,,展开完毕");
+     });
+     $("#clp4").on("hide.bs.collapse",function () {//给这个事件绑定个函数
+         alert("我去哦,关闭前");
+     });
+     $("#clp4").on("hidden.bs.collapse",function () {//给这个事件绑定个函数
+         alert("我去哦,关闭后");
+     });
+ });
 });
