b4d8721a236a0f38fb5a20fd09a8bf4fd9f252f0
zxxiaobai <648236798@qq.com>
2020-05-19 23:36:09 +0800

    //此处需要关注的点，1，修改属性，2，获取自定义事件值         //自定义事件，获取属性添加

--- js/admin.js
+++ js/admin.js
@@ -14,5 +14,15 @@ $(function () {
         i.removeClass('icon-jianhao');
         i.addClass('icon-21');
     });
+    $(".collapse_all >li >a").click(function (el) {
+        el.preventDefault();//禁用当前元素原本功能
+        //$(this) 当前对象
+        var $this=$(this);
+        $(".collapse_all >li >a").removeClass("navactive");//去掉所有之前加过点击的样式
+        $this.addClass("navactive");
+        $("#iframe-contant").attr('src',$this.data('iframesrc'));
+        //此处需要关注的点，1，修改属性，2，获取自定义事件值
+        //自定义事件，获取属性添加
+    });
 
 });
